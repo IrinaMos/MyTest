f4b021397d7759fb35811f202556bd9421286954
Irenamos <irena.moskovich@gmail.com>
2019-03-13 15:40:39 +0200

    click in newbutton

---
+++ Software/src/Click.java
@@ -0,0 +1,11 @@
+public class Click {
+
+    public class Main {
+        public void main(String[] args) {
+
+            System.out.print("Click");
+
+        }
+    }
+}
+C:\Users\irina\IdeaProjects\Software\src\Click.java
