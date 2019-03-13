a9362208da55aacae41a5dca96012ecc93888b53
Irenamos <irena.moskovich@gmail.com>
2019-03-13 17:12:23 +0200

    newscreen

---
+++ Software/src/Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome {
+
+        public class Main {
+            public void main(String[] args) {
+
+                System.out.print("NewScreen");
+
+            }
+        }
+}
