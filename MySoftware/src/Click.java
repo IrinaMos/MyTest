4309e686b53538058fb12ff3f772becd274c093b
Irenamos <irena.moskovich@gmail.com>
2019-03-14 10:45:26 +0200

    click

---
+++ MySoftware/src/Click.java
@@ -0,0 +1,10 @@
+public class Click {
+    public class Main {
+        public void main(String[] args) {
+
+            System.out.print("Click");
+            System.out.print("Click");
+
+        }
+    }
+}
